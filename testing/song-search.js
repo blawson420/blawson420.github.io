10255ebc5d711ca06b853ee520996b4f2b617f31
blawson420 <blawson420@icloud.com>
2023-01-14 17:50:09 -0600

    Update song-search.js

--- testing/song-search.js
+++ testing/song-search.js
@@ -1,46 +1,47 @@
-$.getJSON('songs.json', function(data) {
-    var searchInput = $('#search-input').val();
-    var searchResults = data.filter(function(song) {
-      return song.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
-    });
-    // code to display searchResults in a well-formatted manner
-    var resultsContainer = $('#results');
-    if(searchResults.length > 0){
-        searchResults.forEach(function(song){
-            var songDiv = $('<div>').addClass('song-result');
-            var songTitle = $('<h2>').text(song.title);
-            var songArtist = $('<p>').text(song.artist);
-            var songAlbum = $('<p>').text(song.album);
-            var songCover = $('<img>').attr('src', song.album_cover_url);
-            var songDate = $('<p>').text(song.release_date);
+var allSongs;
+$(document).ready(function(){
+  $.getJSON('songs.json', function(data) {
+    allSongs = data;
+    displaySongs(allSongs);
+  });
+  var searchInput = $('#search-input').val();
+  $('#search-button').click(function(){
+    searchSongs(searchInput);
+  });
+});
 
-            songDiv.append(songTitle);
-            songDiv.append(songArtist);
-            songDiv.append(songAlbum);
-            songDiv.append(songCover);
-            songDiv.append(songDate);
-            resultsContainer.append(songDiv);
-        });
-    }else{
-        resultsContainer.append("<p>No Results Found</p>");
-    }
-  });
-  
-  $(document).ready(function(){
-    $('#search-button').click(function(){
-      var searchInput = $('#search-input').val();
-      // call the function for search the song
-      searchSongs(searchInput);
-    });
-  });
-  
+  var searchInput;
   function searchSongs(searchInput){
     // code to filter and search the songs
-    $.getJSON('songs.json', function(data) {
-    var searchResults = data.filter(function(song) {
+    var searchResults = allSongs.filter(function(song) {
       return song.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
     });
-    // code to display searchResults in a well-formatted manner
-  });
+    console.log(searchResults);
+    displaySongs(searchResults);
   }
   
+  function displaySongs(songs){
+    console.log(songs);
+    // code to display songs in a well-formatted manner
+    var resultsContainer = $('#results');
+    resultsContainer.empty();
+    if(songs.length > 0){
+      songs.forEach(function(song){
+        var songDiv = $('<div>').addClass('song-result');
+        var songTitle = $('<h2>').text(song.title);
+        var songArtist = $('<p>').text(song.artist);
+        var songAlbum = $('<p>').text(song.album);
+        var songCover = $('<img>').attr('src',song.album_cover_url);
+        var songDate = $('<p>').text(song.release_date);
+
+        songDiv.append(songTitle);
+        songDiv.append(songArtist);
+        songDiv.append(songAlbum);
+        songDiv.append(songCover);
+        songDiv.append(songDate);
+        resultsContainer.append(songDiv);
+    });
+  }else{
+    resultsContainer.append("<p>No Results Found</p>");
+  }
+}
