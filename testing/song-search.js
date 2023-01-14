35ce52e037e310444c1cab7a4d6cc3f183c1c6d4
blawson420 <96959717+blawson420@users.noreply.github.com>
2023-01-14 17:11:12 -0600

    Add files via upload

---
+++ testing/song-search.js
@@ -0,0 +1,29 @@
+$.getJSON('songs.json', function(data) {
+    var searchInput = $('#search-input').val();
+    var searchResults = data.filter(function(song) {
+      return song.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
+    });
+    // code to display searchResults in a well-formatted manner
+    var resultsContainer = $('#results');
+    if(searchResults.length > 0){
+        searchResults.forEach(function(song){
+            var songDiv = $('<div>').addClass('song-result');
+            var songTitle = $('<h2>').text(song.title);
+            var songArtist = $('<p>').text(song.artist);
+            var songAlbum = $('<p>').text(song.album);
+            var songCover = $('<img>').attr('src', song.album_cover_url);
+            var songDate = $('<p>').text(song.release_date);
+            
+            songDiv.append(songTitle);
+            songDiv.append(songArtist);
+            songDiv.append(songAlbum);
+            songDiv.append(songCover);
+            songDiv.append(songDate);
+            resultsContainer.append(songDiv);
+        });
+    }else{
+        resultsContainer.append("<p>No Results Found</p>");
+    }
+
+  });
+  
