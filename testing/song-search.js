61313b4aff3211a0dbfc9bf03335e06ac0b650f4
blawson420 <blawson420@icloud.com>
2023-01-14 18:14:24 -0600

    Working Example

--- testing/song-search.js
+++ testing/song-search.js
@@ -1,47 +1,47 @@
-var allSongs;
-$(document).ready(function(){
-  $.getJSON('songs.json', function(data) {
-    allSongs = data;
-    displaySongs(allSongs);
-  });
-  var searchInput = $('#search-input').val();
-  $('#search-button').click(function(){
-    searchSongs(searchInput);
-  });
-});
-
-  var searchInput;
-  function searchSongs(searchInput){
-    // code to filter and search the songs
-    var searchResults = allSongs.filter(function(song) {
-      return song.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
+$(document).ready(function() {
+    $.getJSON("songs.json", function(data) {
+      allSongs = data;
     });
-    console.log(searchResults);
-    displaySongs(searchResults);
-  }
+    
+    var searchInput = document.getElementById("search-input").value;
+    var searchButton = document.getElementById("search-button");
   
-  function displaySongs(songs){
-    console.log(songs);
-    // code to display songs in a well-formatted manner
-    var resultsContainer = $('#results');
-    resultsContainer.empty();
-    if(songs.length > 0){
-      songs.forEach(function(song){
-        var songDiv = $('<div>').addClass('song-result');
-        var songTitle = $('<h2>').text(song.title);
-        var songArtist = $('<p>').text(song.artist);
-        var songAlbum = $('<p>').text(song.album);
-        var songCover = $('<img>').attr('src',song.album_cover_url);
-        var songDate = $('<p>').text(song.release_date);
-
-        songDiv.append(songTitle);
-        songDiv.append(songArtist);
-        songDiv.append(songAlbum);
-        songDiv.append(songCover);
-        songDiv.append(songDate);
-        resultsContainer.append(songDiv);
-    });
-  }else{
-    resultsContainer.append("<p>No Results Found</p>");
-  }
-}
+    searchButton.addEventListener("click", searchSongs);
+  
+    function searchSongs() {
+      searchInput = document.getElementById("search-input").value;
+      var filteredSongs = allSongs.filter(function(song) {
+        return (
+          song.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+          song.artist.toLowerCase().includes(searchInput.toLowerCase())
+        );
+      });
+      var results = document.getElementById("results");
+      results.innerHTML = "";
+      if (filteredSongs.length === 0) {
+        results.innerHTML = "No Results Found";
+      } else {
+        for (var i = 0; i < filteredSongs.length; i++) {
+          results.innerHTML +=
+            "<div class='song'>" +
+            "<img src='" +
+            filteredSongs[i].album_cover_url +
+            "'/>" +
+            "<h2>" +
+            filteredSongs[i].title +
+            "</h2>" +
+            "<p>" +
+            filteredSongs[i].artist +
+            "</p>" +
+            "<p>" +
+            filteredSongs[i].album +
+            "</p>" +
+            "<p>" +
+            filteredSongs[i].release_date +
+            "</p>" +
+            "</div>";
+            }
+            }
+            }
+            });
+  
